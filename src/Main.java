b3979cfb4c6ba727eecebb8ebfaf0c293d3f58ea
Ma7you21 <mallem.mahieddine@gmail.com>
2024-08-21 15:59:14 -0400

    my second commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello syntax");
         System.out.println("hello day");
+        System.out.println("hello day2 ");
 
 
     }
